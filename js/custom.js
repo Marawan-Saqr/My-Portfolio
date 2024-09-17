daf6641d77691f4e5540e90fdb65785521218385
Marawan Saqr <loew.krs@yahoo.com>
2024-09-17 17:41:41 +0300

    First Version

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// News section function
+let style = document.createElement('style');
+let position = "right";
+style.innerHTML = `@keyframes my-animation {
+  0%{${position}: -${document.querySelector('.text').offsetWidth + 10}px;}
+  100%{${position}: 100%;}
+}`;
+document.head.append(style);
+
+// WOW JS Function
+new WOW().init();
